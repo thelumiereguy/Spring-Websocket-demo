325e5f03d39144d628d5f8183ae29be27b9eb8e5
thelumiereguy <46375353+thelumiereguy@users.noreply.github.com>
2021-03-18 21:56:43 +0530

    cleaned up code

--- src/main/kotlin/com/thelumiereguy/websocket_demo/controller/NotesController.kt
+++ src/main/kotlin/com/thelumiereguy/websocket_demo/controller/NotesController.kt
@@ -1,14 +1,11 @@
 package com.thelumiereguy.websocket_demo.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.thelumiereguy.websocket_demo.model.Note
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.SimpMessagingTemplate
-import org.springframework.messaging.simp.annotation.SubscribeMapping
-import org.springframework.messaging.support.GenericMessage
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
